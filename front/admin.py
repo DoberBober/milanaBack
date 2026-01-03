0b8eac3bf8a7e4a6c96009aed4ed5cadc0da1bc9
DoberBober <pochta.q@bk.ru>
2026-01-04 01:17:54 +0300

    Models.

--- front/admin.py
+++ front/admin.py
@@ -1,16 +1,44 @@
 from django.contrib import admin
 
-from front.models import Front
+from front.models import Front, Diplom, Review, Page, Post, Service, Faq, Order
 
 
-# Register your models here.
 @admin.register(Front)
 class FrontAdmin(admin.ModelAdmin):
 	list_display = ['title',]
 
-	fields = (
-		'title',
-		('front_seo_title', 'front_seo_description',),
+	fieldsets = (
+		(None, {
+			'fields': (
+				'title',
+			)
+		}),
+		('SEO-настройки', {
+			'fields': (
+				('front_seo_title', 'front_seo_description',),
+				('about_seo_title', 'about_seo_description',),
+				('reviews_seo_title', 'reviews_seo_description',),
+				('articles_seo_title', 'articles_seo_description',),
+			)
+		}),
+		('Контактные данные', {
+			'fields': (
+				('phone', 'email',),
+				'vk',
+				'wa',
+				'tg',
+				'ok',
+			)
+		}),
+		('О враче', {
+			'fields': (
+				('front_heading', 'front_desc',),
+				'photo',
+				'about_desc',
+				'about_text',
+				('point_1', 'point_2',),
+			)
+		}),
 	)
 
 	def has_delete_permission(self, request, obj=None):
@@ -20,3 +48,93 @@ class FrontAdmin(admin.ModelAdmin):
 		if Front.objects.exists():
 			return False
 		return True
+
+
+@admin.register(Diplom)
+class DiplomAdmin(admin.ModelAdmin):
+	list_display = ['title', 'show_on_front',]
+
+	fields = (
+		'show_on_front',
+		'title',
+		'photo',
+	)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+	list_display = ['title', 'show_on_front',]
+
+	fields = (
+		'show_on_front',
+		'title',
+		'text',
+	)
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+	list_display = ['title',]
+
+	fields = (
+		'seo_title',
+		'seo_description',
+		'title',
+		'text',
+	)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['title',]
+
+	fields = (
+		('meta_title', 'meta_description',),
+		'created',
+		'type',
+		'title',
+		'desc',
+		'text',
+		'thumbnail',
+		'video',
+		'photo',
+	)
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+	list_display = ['title',]
+
+	fields = (
+		('meta_title', 'meta_description',),
+		'created',
+		('show_on_front', 'favorite',),
+		('empty', 'parent',),
+		'title',
+		'text',
+		'photo',
+	)
+
+
+@admin.register(Faq)
+class FaqAdmin(admin.ModelAdmin):
+	list_display = ['question',]
+
+	fields = (
+		'public',
+		'services',
+		'question',
+		'answer',
+	)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+	list_display = ['name', 'phone', 'url', 'created',]
+
+	fields = (
+		'created',
+		'url',
+		'name',
+		'phone',
+	)
--- front/models.py
+++ front/models.py
@@ -1,11 +1,44 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from tinymce import models as tinymce_models
+
+from _conf.models import BaseModel
+
+
 class Front(models.Model):
 	title = models.CharField(blank=False, max_length=256, verbose_name='Название')
 
 	front_seo_title = models.CharField(blank=True, default='', max_length=256, verbose_name='СЕО заголовок для главной')
 	front_seo_description = models.CharField(blank=True, default='', max_length=256, verbose_name='СЕО описание для главной')
+
+	about_seo_title = models.CharField(blank=True, default='', max_length=256, verbose_name='СЕО заголовок для страницы "О враче"')
+	about_seo_description = models.CharField(blank=True, default='', max_length=256, verbose_name='СЕО описание для страницы "О враче"')
+
+	reviews_seo_title = models.CharField(blank=True, default='', max_length=256, verbose_name='СЕО заголовок для страницы "Отзывы"')
+	reviews_seo_description = models.CharField(blank=True, default='', max_length=256, verbose_name='СЕО описание для страницы "Отзывы"')
+
+	articles_seo_title = models.CharField(blank=True, default='', max_length=256, verbose_name='СЕО заголовок для страницы "Статьи"')
+	articles_seo_description = models.CharField(blank=True, default='', max_length=256, verbose_name='СЕО описание для страницы "Статьи"')
+
+	phone = models.CharField(blank=True, default='', max_length=256, verbose_name='Телефон')
+	email = models.CharField(blank=True, default='', max_length=256, verbose_name='Почта')
+
+	vk = models.CharField(blank=True, default='', max_length=256, verbose_name='Вконтакте')
+	wa = models.CharField(blank=True, default='', max_length=256, verbose_name='Whatsapp')
+	tg = models.CharField(blank=True, default='', max_length=256, verbose_name='Telegram')
+	ok = models.CharField(blank=True, default='', max_length=256, verbose_name='Одноклассники')
+
+	photo = models.ImageField(upload_to='front/', blank=False, verbose_name='Фото врача', help_text="900x732")
+	front_heading = models.CharField(blank=False, default='', max_length=256, verbose_name='Заголовок на главной')
+	front_desc = models.CharField(blank=False, default='', max_length=256, verbose_name='Вводный текст на главной')
+
+	about_desc = tinymce_models.HTMLField(verbose_name='Текст в блоке "О враче"')
+	point_1 = models.CharField(blank=True, default='', max_length=256, verbose_name='Пункт 1 в блоке "О враче"', help_text='Для выделения большими - тег <b>')
+	point_2 = models.CharField(blank=True, default='', max_length=256, verbose_name='Пункт 2 в блоке "О враче"', help_text='Для выделения большими - тег <b>')
+
+	about_text = tinymce_models.HTMLField(verbose_name='Текст на странице "О враче"')
+
 
 	class Meta:
 		db_table = '_front'
@@ -14,3 +47,130 @@ class Front(models.Model):
 
 	def __str__(self):
 		return self.title
+
+
+class Diplom(models.Model):
+	show_on_front = models.BooleanField(default=True, verbose_name='Отображать на главной')
+	title = models.CharField(blank=False, max_length=256, verbose_name='Название')
+	photo = models.ImageField(upload_to='front/', blank=True, verbose_name='Фото врача', help_text="530x380")
+
+
+	class Meta:
+		db_table = 'diploms'
+		verbose_name = 'Диплом'
+		verbose_name_plural = 'Дипломы'
+
+	def __str__(self):
+		return self.title
+
+
+class Review(models.Model):
+	show_on_front = models.BooleanField(default=True, verbose_name='Отображать на главной')
+	title = models.CharField(blank=False, max_length=256, verbose_name='Название')
+	text = models.TextField(blank=True, verbose_name='Текст отзыва')
+
+
+	class Meta:
+		db_table = 'reviews'
+		verbose_name = 'Отзыв'
+		verbose_name_plural = 'Отзывы'
+
+	def __str__(self):
+		return self.title
+
+
+class Page(models.Model):
+	seo_title = models.CharField(blank=True, default='', max_length=256, verbose_name='СЕО заголовок')
+	seo_description = models.CharField(blank=True, default='', max_length=256, verbose_name='СЕО описание')
+	title = models.CharField(blank=False, max_length=256, verbose_name='Название')
+	text = tinymce_models.HTMLField(verbose_name='Текст')
+
+
+	class Meta:
+		db_table = 'pages'
+		verbose_name = 'Страница'
+		verbose_name_plural = 'Страницы'
+
+	def __str__(self):
+		return self.title
+
+
+class Post(BaseModel):
+	TYPES = (
+		("article", "Статья"),
+		("video", "Видео"),
+	)
+
+	type = models.CharField(max_length=128, verbose_name='Тип поста', choices=TYPES, default=TYPES[0])
+	title = models.CharField(blank=False, max_length=256, verbose_name='Название')
+	desc = models.TextField(blank=True, verbose_name='Вводный текст')
+	text = tinymce_models.HTMLField(verbose_name='Текст')
+	thumbnail = models.ImageField(upload_to='posts/', blank=False, verbose_name='Картинка в списке', help_text="760x480")
+	video = models.TextField(blank=True, verbose_name='Видео')
+	photo = models.ImageField(upload_to='posts/', blank=True, verbose_name='Картинка в статье', help_text="2360x800")
+
+	class Meta:
+		db_table = 'articles'
+		verbose_name = 'Пост'
+		verbose_name_plural = 'Посты'
+		ordering = ['-created',]
+
+	def __str__(self):
+		return self.title
+
+
+class Service(BaseModel):
+	show_on_front = models.BooleanField(default=True, verbose_name='Отображать на главной')
+	empty = models.BooleanField(default=False, verbose_name='Не открывать страницу')
+	favorite = models.BooleanField(default=False, verbose_name='Выделить на странцие услуг')
+
+	parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='childs', verbose_name='Родительская услуга')
+
+	title = models.CharField(blank=False, max_length=256, verbose_name='Название')
+	text = tinymce_models.HTMLField(verbose_name='Текст')
+	photo = models.ImageField(upload_to='services/', blank=True, verbose_name='Картинка в статье', help_text="2360x800")
+
+	class Meta:
+		db_table = 'services'
+		verbose_name = 'Услуга'
+		verbose_name_plural = 'Услуги'
+		ordering = ['title',]
+
+	def __str__(self):
+		return self.title
+
+
+class Faq(BaseModel):
+	services = models.ManyToManyField(
+		Service,
+		blank=True,
+		related_name='faq',
+		verbose_name='В каких устугах отображать вопрос'
+	)
+
+	question = models.CharField(blank=False, max_length=512, verbose_name='Вопрос')
+	answer = tinymce_models.HTMLField(verbose_name='Ответ')
+
+	class Meta:
+		db_table = 'faq'
+		verbose_name = 'Вопрос-ответ'
+		verbose_name_plural = 'Вопросы-ответы'
+
+	def __str__(self):
+		return self.question
+
+
+class Order(models.Model):
+	created = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
+	url = models.CharField(blank=False, max_length=1024, verbose_name='URL')
+	name = models.CharField(blank=False, max_length=512, verbose_name='Имя')
+	phone = models.CharField(blank=False, max_length=512, verbose_name='Телефон')
+
+	class Meta:
+		db_table = 'orders'
+		verbose_name = 'Запись'
+		verbose_name_plural = 'Записи'
+		ordering = ['-created',]
+
+	def __str__(self):
+		return f'{self.name} {self.phone}'
